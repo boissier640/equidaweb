129b96e4f0222275931b0d9d7bd9568ed88c9b58
zakina <zakina.rostand@gmail.com>
2025-09-08 10:49:56 +0200

    premiere version de equida web sous netbeans avec lister les chevaux, consulter un cheval, ajouter un cheval

---
+++ src/main/java/test/TestConnexionBdd.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import database.Connexionbdd;
+import database.DaoCheval;
+
+public class TestConnexionBdd {
+
+    public static void main (String args[]) {
+
+        Connection cnx = Connexionbdd.ouvrirConnexion();
+        System.out.println ("nombre de chevaux = " + DaoCheval.getLesChevaux(cnx).size());
+
+
+            }
+
+}
